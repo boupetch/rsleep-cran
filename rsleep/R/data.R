4166bbd9a14c7e7ba142fe871a48b6e138a0aef1
Paul Bouchequet <paul.bouchequet@gmail.com>
2020-05-09 15:38:13 +0200

    first commit

---
+++ rsleep/R/data.R
@@ -0,0 +1,13 @@
+#' Sample electrocardiogram signal
+#'
+#' 10 seconds of ECG from Resmed Nox A1 polysomnograph sampled at 200 Hz expressed in Volts.
+#'
+#' @format A vector of 2000 values.
+#' @source \url{http://www.sommeil-vigilance.fr/}
+"example_ecg_200hz"
+
+#' Example hypnogram scored on 30 seconds.
+#'
+#' @format Dataframe
+#'
+"example_hypnogram_30s"
